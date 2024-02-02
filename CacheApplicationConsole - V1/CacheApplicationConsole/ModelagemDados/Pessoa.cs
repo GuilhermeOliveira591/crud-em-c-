ba18e8ebc026a82b7721f109d1fcdc02166d46f4
Guilherme Gomes <65633355+GuilhermeOliveira591@users.noreply.github.com>
2024-02-02 14:47:37 -0300

    Delete CacheApplicationConsole - V1 directory

--- CacheApplicationConsole - V1/CacheApplicationConsole/ModelagemDados/Pessoa.cs
+++
@@ -1,9 +0,0 @@
-public class Pessoa
-{
-    public int Id { get; set; }
-    public string NomeCompleto { get; set; }
-    public string Genero { get; set; }
-    public string CPF { get; set; }
-    public string EnderecoCompleto { get; set; }
-    public int RedisIdentifier { get; set; }
-}
--- CacheApplicationConsole - V1/README.txt
+++
@@ -1,2 +0,0 @@
-Instalar pacote -> Install-Package StackExchange.Redis
-Instalar pacote -> Install-Package Newtonsoft.Json [Documentation at: https://stackexchange.github.io/StackExchange.Redis/Basics]
